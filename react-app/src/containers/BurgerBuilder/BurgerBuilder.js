0ec0abaa4de330ad21f782ae96b88029398a1ed3
Ruturaj More <ruturajmore1234@gmail.com>
2020-06-29 20:48:42 +0530

    master: 134. Connecting State to Build Controls

--- react-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,14 +3,50 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 50,
+  bacon: 80,
+  cheese: 40,
+  meat: 100
+};
+
 class BurgerBuilder extends Component{
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 80
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const newCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = newCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceAddition;
+    this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
+  }
+
+  removeIngredientHandler = (type) => {
+    if(this.state.ingredients[type] > 0){
+      const oldCount = this.state.ingredients[type];
+      const newCount = oldCount - 1;
+      const updatedIngredients = {
+        ...this.state.ingredients
+      };
+      updatedIngredients[type] = newCount;
+      const priceSubstraction = INGREDIENT_PRICES[type];
+      const oldPrice = this.state.totalPrice;
+      const newPrice = oldPrice - priceSubstraction;
+      this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
     }
   }
 
@@ -18,7 +54,9 @@ class BurgerBuilder extends Component{
     return(
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls/>
+        <BuildControls
+          ingredientAdded = {this.addIngredientHandler}
+        />
       </Aux>
     );
   }
