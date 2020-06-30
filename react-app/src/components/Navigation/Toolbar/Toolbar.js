d3358e443fbf33a2e00d4d70a6be34fb942c5cde
Ruturaj More <ruturajmore1234@gmail.com>
2020-06-30 10:41:09 +0530

    master: 148. Working on Responsive Adjustments

--- react-app/src/components/Navigation/Toolbar/Toolbar.js
+++ react-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <Logo/>
+    <div className={classes.Logo}>
+      <Logo/>
+    </div>
     <nav>
       <NavigationItems/>
     </nav>
